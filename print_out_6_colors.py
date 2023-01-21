2bd6b6c537b2c2d5c1cd8eba659138af5fd19712
rastawarlord <ctf.rasta.warlord@gmail.com>
2023-01-21 02:31:13 -0600

    new file:   Python 	modified:   print_out_6_colors.py

--- print_out_6_colors.py
+++ print_out_6_colors.py
@@ -1,6 +1,10 @@
 #Print out 6 colors in a non repeating pattern
+import itertools
 
 colors = ['yellow', 'green', 'blue', 'red', 'pink', 'orange']
 
-for col in colors:
-    
+gem = [p for p in itertools.permutations(colors, 6)]
+
+for i in gem:
+#   print("Number of permutations are {} ".format(str(len(gem))))
+    print(str(i))
