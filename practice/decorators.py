591cdadbe74cfe94c33310484f7408cbc5ed333d
adminuser <adminuser@live.lan>
2024-09-12 09:02:23 -0500

    Updated decorators.py and useDecorators.py

--- practice/decorators.py
+++ practice/decorators.py
@@ -12,3 +12,18 @@ def timer(func):
 		print(f"Finished {func.__name__}() in {run_time:.4f} secs")
 		return value
 	return wrapper_timer
+
+
+def debug(func):
+	"""Print the function signature and return value"""
+	@functools.wraps(func)
+	def wrapper_debug(*args, **kwargs):
+		args_repr = [repr(a) for a in args]
+		kwargs_repr = [f"{k}={repr(v)}" for k, v in kwargs.items()]
+		signature = ", ".join(args_repr + kwargs_repr)
+		print(f"Calling {func.__name__}({signature})")
+		value = func(*args, **kwargs)
+		print(f"{func.__name__}() returned {repr(value)}")
+		return value
+	return wrapper_debug
+
--- practice/useDecorators.py
+++ practice/useDecorators.py
@@ -1,4 +1,6 @@
+import math
 from decorators import timer
+from decorators import debug
 
 @timer
 def waste_some_time(num_times):
@@ -9,3 +11,27 @@ waste_some_time(1)
 
 waste_some_time(999)
 
+print()
+print("#################################")
+print()
+
+@debug
+def make_greeting(name, age=None):
+	if age is None:
+		return f"Hello {name}"
+	else:
+		return f"You are the {name} at {age} already!"
+
+make_greeting("rasta")
+make_greeting("warl0rds", age=43)
+
+print()
+print("#################################")
+print()
+
+math.factorial = debug(math.factorial)
+
+def approximate_e(terms=18):
+	return sum(1 / math.factorial(n + 1) for n in range(terms))
+
+approximate_e(10)
